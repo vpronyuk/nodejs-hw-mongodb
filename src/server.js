049fdbbba8849ed40992edef934b0abe8a7243b6
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-07-10 23:09:45 +0300

    start code for server.js + added utils

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
