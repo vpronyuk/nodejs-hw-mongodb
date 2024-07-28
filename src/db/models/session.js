38a62a8d91921acae705b61486fc6371e34c9585
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-07-28 23:27:02 +0300

    create models User and Session

---
+++ src/db/models/session.js
@@ -0,0 +1,17 @@
+import { model, Schema } from 'mongoose';
+
+const sessionsSchema = new Schema(
+  {
+    userId: { type: String, required: true },
+    accessToken: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+    accessTokenValidUntil: { type: Date, required: true },
+    refreshTokenValidUntil: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Session = model('sessions', sessionsSchema);
