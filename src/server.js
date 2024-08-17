33f951c6424ab5054fdb7f29c17401cdb8a6a2b4
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-08-18 00:09:19 +0300

    fix locations

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import router from './routers/index.js';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { swaggerDocs } from './middlewares/swaggerDocs.js';
+// import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(env('PORT', '3000'));
@@ -19,7 +19,7 @@ export const setupServer = () => {
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
+  // app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
